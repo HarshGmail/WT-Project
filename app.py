4fad453076ff2f444bc19d8b315960d1282189b0
HarshGmail <madala.harshvardhan160801@gmail.com>
2022-03-15 20:47:03 +0530

    added login page

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Khatabook_Database.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
+
+@app.route("/",methods=["GET","POST"])
 def loginpage():
     return render_template("login.html")
 
 if __name__=="__main__":
     app.run(debug=True)
-    
